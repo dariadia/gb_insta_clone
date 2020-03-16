027f14076420c31bbfad71bb0b5fbf411d3fd010
Maxi-M <maxim.moskovskiy@mail.ru>
2020-03-16 18:11:13 +0300

    Создание таблицы Comments

--- config/web.php
+++ config/web.php
@@ -16,7 +16,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-//            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'b8zoXoS0gVz4x-gGjCFPnJZ5n9FyMC-f',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
