0cfe62ae190a617508b6035b641622241dc40e70
Cheef <38507648+Cheefs@users.noreply.github.com>
2020-03-18 18:12:20 +0700

    Update web.php
    [fix] поправил файл, вернул исходный ( так как произошло дублирование параметров )

--- config/web.php
+++ config/web.php
@@ -1,45 +1,36 @@
 <?php
 
 $params = require __DIR__ . '/params.php';
-$paramsLocal = require __DIR__ . '/params-local.php';
+
+$configLocal = require __DIR__ . '/web-local.php';
+
 $db = require __DIR__ . '/db.php';
 $dbLocal = require __DIR__ . '/db-local.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'api',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'basePath' => '@api/modules/v1',
+            'class' => \api\modules\v1\Module::class
+        ],
+    ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        '@api'   => dirname(dirname(__DIR__)) . '/api',
     ],
     'components' => [
         'request' => [
             'parsers' => [
-                'application/json' => 'yii\web\JsonParser',
-            ]
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-//            'cookieValidationKey' => '',
-            'parsers' => [
                 'application/json' => \yii\web\JsonParser::class,
             ]
         ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'enableAutoLogin' => false,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -53,30 +44,18 @@ $config = [
         'db' => array_merge($db + $dbLocal),
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
+            'rules' => [
+                [
+                    'controller' => ['v1/media'],
+                    'class' => \yii\rest\UrlRule::class,
+                ],
+            ],
         ],
-    ],
-    'modules' => [
 
     ],
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
-    ];
-}
-
-return array_merge_recursive($paramsLocal, $config);
+return array_merge_recursive($configLocal, $config);
