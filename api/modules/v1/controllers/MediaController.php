1cd1f6d77c83e1f971df433ec9d084b68dc371b4
cheefs <cheefst@outlook.com>
2020-03-18 17:59:13 +0700

    [*] перенес mediaContoller в директорию API

--- api/modules/v1/controllers/MediaController.php
+++ api/modules/v1/controllers/MediaController.php
@@ -1,62 +1,27 @@
 <?php
 
-namespace app\controllers;
+namespace api\modules\v1\controllers;
 
 use Yii;
-use yii\helpers\Url;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 
 /**
  * Контроллер работы с media
  * не реализовывал метод для получения данных на клиен в виде json ( для реализации пагинаций, на клиенте) так как пока не ясно как отрисовка страниц будет
-*/
+ */
 class MediaController extends Controller
 {
     const MEDIA_PAGING_LIMIT = 20;
-    /**
-     * По хорошему нужно сделать модуль User -> в него вынести меди контроллер, и будет имитация перехода на страницу
-     * http://site/user/:id/media
-     *
-     * Получить весь медиа контент с базы
-     * @return string
-     **/
-    public function actionIndex()
-    {
-        $mediaList = /* (new Media())->find()->all(); */ []; /// Заменить на получение
-        return $this->render('index', [
-            'mediaList' => $mediaList
-        ]);
-    }
-
-    /**
-     * Просмотр определенного контента - возможно это уйдет на плечи фронта
-     * @param int $mediaId
-     * @return string
-     **/
-    public function actionView( int $mediaId )
-    {
-        $selectedMedia = /* ( new MediaModel() )->findOne([ 'id' => $mediaId ]) */ null;
-
-        if ( !$selectedMedia ) {
-            return $this->redirect( Url::to(['site/404']) );
-        }
-
-        return $this->render('view', [
-            'selectedMedia' => $selectedMedia
-        ]);
-    }
 
     /**
      * Имитация возможности поставить лайк, через асинхронный запрос
      * @method POST
      * @example
-     *   const data = await fetch('http://localhost/media/like', {
+     *   const data = await fetch('http://localhost/v1/media/like', {
      *   method: 'POST',
      *   headers: {
      *        "Content-Type": "application/json",
-     *        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-     *        "X-Requested-With": "XMLHttpRequest",
      *    },
      *    body: JSON.stringify({
      *        mediaId: 1
@@ -89,12 +54,10 @@ class MediaController extends Controller
      * @param int $offset указатель на страницу медиа ( для пагинации если будем реализовывать )
      * @param int $limit необязательный параметр, указывает на колличество записей в ленте
      * @example
-     *     const response = await fetch('http://localhost/media/list?userId=1&offset=5', {
+     *     const response = await fetch('http://localhost/v1/media/list?userId=1&offset=5', {
      *           method: 'GET',
      *           headers: {
      *               "Content-Type": "application/json",
-     *               "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-     *               "X-Requested-With": "XMLHttpRequest",
      *            },
      *       });
      *     const data = await data.json()
@@ -110,7 +73,6 @@ class MediaController extends Controller
                     ->offset( $offset )
                     ->all()
             */
-
             return $this->asJson( /* $querry*/ []);
         }
         return $this->redirect('/media');
--- views/media/index.php
+++
@@ -1,2 +0,0 @@
-<?php ?>
-    <div id="media-content"></div>
