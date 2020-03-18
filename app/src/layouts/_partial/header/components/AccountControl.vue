e39405d6a482434ad081451f4583752d84dc3dee
cheefs <cheefst@outlook.com>
2020-03-18 19:18:36 +0700

    [*] добавил наброски для будующего template, и подключил к нему header

---
+++ app/src/layouts/_partial/header/components/AccountControl.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="accout__control">
+    <div v-if="isGuest" class="guest__buttons">
+      <button class="waves-effect waves-light btn-small">Login</button>
+      <button class="btn-flat">Register</button>
+    </div>
+    <div v-if="!isGuest" class="user__buttons">
+        <button class="waves-effect waves-red btn-small">
+           <i class="material-icons">exit_to_app</i>
+        </button>
+    </div>
+    
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data: function() {
+      return {
+        isGuest: false, // настроить передачу через props из state приложения,
+        user: {} // аналогично ключу isGuest
+      }
+    },
+  }
+</script>
