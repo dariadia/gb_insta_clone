d9289e88c5faef130f215b7b26fc47ad89f7216d
Maxi-M <maxim.moskovskiy@mail.ru>
2020-03-18 17:41:24 +0300

    'Comments'

--- migrations/m200316_144523_create_comments_table.php
+++ migrations/m200316_144523_create_comments_table.php
@@ -3,16 +3,29 @@
 use yii\db\Migration;
 
 /**
- * Class m200316_144523_add_comments_table
+ * Creates the comments table and foreign keys to media and users table
+ * Class m200316_144523_create_comments_table
  */
-class m200316_144523_add_comments_table extends Migration
+class m200316_144523_create_comments_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+        $this->createTable('{{%comments}}', [
+            'id' => $this->primaryKey(),
+            'author_id' => $this->bigInteger()->notNull(),
+            'media_id' => $this->bigInteger()->notNull(),
+            'comment' => $this->text()->notNull(),
+            'created_at' => $this->timestamp()->defaultExpression('now()'),
+            'updated_at' => $this->timestamp()->defaultExpression('now()'),
+        ]);
+        /*$this->addForeignKey('fk_comments_media_id', '{{%comments}}', 'media_id',
+            '{{%media}}', 'id');*/
 
+        /*$this->addForeignKey('fk_comments_author_id', '{{%comments}}', 'author_id',
+            '{{%users}}', 'id');*/
     }
 
     /**
@@ -20,23 +33,10 @@ class m200316_144523_add_comments_table extends Migration
      */
     public function safeDown()
     {
-        echo "m200316_144523_add_comments_table cannot be reverted.\n";
+        //$this->dropForeignKey('fk_comments_media_id', '{{%comments}}');
+        //$this->dropForeignKey('fk_comments_author_id', '{{%comments}}');
 
-        return false;
+        $this->dropTable('{{%comments}}');
     }
 
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m200316_144523_add_comments_table cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
