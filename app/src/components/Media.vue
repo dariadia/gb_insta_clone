f1f00e9a2e2ccac920217260b54f6c5a77a9954d
Daria Diachkova <daria.diachkova@mail.ru>
2020-03-23 10:21:38 +0300

    add date, alter Media.vue to fit Model

--- app/src/components/Media.vue
+++ app/src/components/Media.vue
@@ -2,33 +2,32 @@
   <div class="media">
     <div class="header level">
       <figure class="image is-32x32">
-<!--        <img :src="media.userImage" />-->
+        <!--        <img :src="media.userImage" />-->
       </figure>
       <span class="username">{{media.username}}</span>
     </div>
     <div
       v-if="media.type === 'image'"
       class="image-container"
-      :style="{ backgroundImage: 'url(' + media.src + ')' }"
+      :style="{ backgroundImage: 'url(' + media.filename + ')' }"
     ></div>
     <div v-else class="video-container">
       <video controls>
-        <source :src="media.src" type="video/mp4" />Your browser does not support HTML5 video.
+        <source :src="media.filename" type="video/mp4" />Your browser does not support HTML5 video.
       </video>
     </div>
     <div class="content">
       <div class="heart">
-        <i class="far fa-heart fa-lg" :class="{'fas': this.media.hasBeenLiked}">
-          <!-- @click="like"-->
-        </i>
+        <i class="far fa-heart fa-lg" :class="{'fas': this.media.hasBeenLiked}" @click="like"></i>
       </div>
       <div>
-<!--        <p class="likes">{{media.likes}} likes</p>-->
+        <p class="likes">{{media.likes}} likes</p>
         <p class="comments">{{media.comments}} comments</p>
+        <p>{{getDate(media.created_at)}}</p>
       </div>
       <p class="caption">
         <span>{{media.username}}</span>
-        {{media.body}} <!-- оно же caption, как понимаю -->
+        {{media.body}}
       </p>
     </div>
   </div>
@@ -39,18 +38,22 @@ export default {
   name: "Media",
   props: {
     media: Object
+  },
+  methods: {
+    like() {
+      this.media.hasBeenLiked ? this.media.likes-- : this.media.likes++;
+      this.media.hasBeenLiked = !this.media.hasBeenLiked;
+    },
+    //  comment() {
+    //    (might cause an issue, so TODO)
+    //    this.media.hasBeenCommented ? this.media.comments-- : this.media.comments++;
+    //     this.media.hasBeenCommented = !this.media.hasBeenCommented;
+    //   }
+    getDate(timestamp) {
+      let date = new Date(timestamp);
+      return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+    }
   }
-  // methods: {
-  //   like() {
-  //     this.media.hasBeenLiked ? this.media.likes-- : this.media.likes++;
-  //     this.media.hasBeenLiked = !this.media.hasBeenLiked;
-  //   }
-  //  comment() {
-  //    (might cause an issue, so TODO)
-  //    this.media.hasBeenCommented ? this.media.comments-- : this.media.comments++;
-  //     this.media.hasBeenCommented = !this.media.hasBeenCommented;
-  //   }
-  // }
 };
 </script>
 
