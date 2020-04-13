3eefee13c7c03e10929c404d7873cc89c0fa83b6
cheefs <cheefst@outlook.com>
2020-04-13 21:22:02 +0700

    [-] удалил закоменнтированные строки

--- modules/v1/controllers/AuthController.php
+++ modules/v1/controllers/AuthController.php
@@ -49,18 +49,7 @@ class AuthController extends ActiveController
                     'Access-Control-Expose-Headers' => [ '*' ],
                     'Access-Control-Request-Headers' => [ '*' ],
                     'Access-Control-Allow-Origin' => ['*'],
-                    // restrict access to
-
-                    // Allow only POST and PUT methods
                     'Access-Control-Request-Method' => ['POST', 'PUT', 'GET', 'OPTIONS'],
-                    // Allow only headers 'X-Wsse'
-//                    'Access-Control-Request-Headers' => ['X-Wsse'],
-                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
-//                    'Access-Control-Allow-Credentials' => true,
-                    // Allow OPTIONS caching
-//                    'Access-Control-Max-Age' => 3600,
-                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-//                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
                 ],
             ]
         ]);
